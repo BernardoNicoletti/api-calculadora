307545678ebf3900bc60e12742f15287b5f01efd
Bernardo Nicoletti <benicoletti2@gmail.com>
2026-08-31 21:31:35 -0300

    Initial commit

---
+++ api-calculadora/src/test/java/com/nicoletti/api_calculadora/ApiCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicoletti.api_calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
